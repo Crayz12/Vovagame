95f4646147f46c447732a3f341577b2bf383a894
Crayz12 <70708880+Crayz12@users.noreply.github.com>
2022-06-12 14:56:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# УГАДАЙ ЧИСЛО =)
+# угадай число =)
 import random
 import time
 from colorama import init
@@ -6,39 +6,43 @@ from colorama import Fore, Back, Style
 init()
 # Чит-код для игры
 # Попытки и время
+# удалить брик(кирпич)
 random_number = random.randint(1, 100)
+input_number = 0
 
 
-def get_user_number():
+def check_number(input_number):
+    input_number = input()
     while True:
         try:
-            print(Fore.YELLOW)
-            return int(input("The program randomly selected a number from 1 to 100, your task is to guess this number with small hints. Please enter a number:"))
+            return int(input_number)
         except ValueError:
-            print(Fore.BLACK)
-            print("Invalid input. Please try again!")
-
-
-user_number = get_user_number()
-print("Thanks! You entered: {0:d}".format(user_number))
+            print(Fore.MAGENTA)
+            input_number = input("Invalid input. Please try again!\n")
+            break
+            
+            
+            
+print(Fore.YELLOW)
+print("The program randomly selected a number from 1 to 100, your task is to guess this number with small hints. Please enter a number:")
+user_number = check_number(input_number)
+print(Fore.YELLOW)
+print(f"Thanks! You entered: {user_number}")
 
 while user_number != random_number:
-
-    try:
-
     if int(user_number) > random_number:
         print(Fore.GREEN)
         print(f"Hidden number is lower than {user_number}")
         print(Fore.YELLOW)
-        user_number = input("Choose the number from 1 to 100: ")
+        print("Choose the number from 1 to 100: ")
+        check_number(input_number)
 
     elif int(user_number) < random_number:
         print(Fore.RED)
         print(f"Hidden number is BIGGER than {user_number}")
         print(Fore.YELLOW)
-        user_number = input("Choose the number from 1 to 100: ")
-
-
+        print("Choose the number from 1 to 100: ")
+        
 
     else:
         print(Fore.CYAN)
